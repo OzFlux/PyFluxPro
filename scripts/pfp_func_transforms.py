63eb3793943ea080e7d948a36ec36634d9efb731
Peter Isaac <pisaac.ozflux@gmail.com>
2023-08-15 19:04:58 +1000

    Changes functions for calculating Ws and Wd from Ux and Uy.

--- scripts/pfp_func_transforms.py
+++ scripts/pfp_func_transforms.py
@@ -16,10 +16,16 @@ def Ws_from_Ux_Uy(ds, Ws_out, Ux_in, Uy_in):
     Author: PRI
     Date: August 2023
     """
+    nrecs = int(ds.root["Attributes"]["nc_nrecs"])
+    zeros = numpy.zeros(nrecs, dtype=numpy.int32)
+    ones = numpy.ones(nrecs, dtype=numpy.int32)
     Ux = pfp_utils.GetVariable(ds, Ux_in)
     Uy = pfp_utils.GetVariable(ds, Uy_in)
-    Ws, _ = pfp_utils.convert_UVtoWSWD(Ux, Uy)
-    Ws["Label"] = Ws_out
+    attr = {"long_name": "Wind speed", "units": "m/s",
+            "statistic_type": "average"}
+    Ws = pfp_utils.CreateEmptyVariable(Ws_out, nrecs, attr=attr)
+    Ws["Data"] = numpy.ma.sqrt(Ux["Data"]*Ux["Data"] + Uy["Data"]*Uy["Data"])
+    Ws["Flag"] = numpy.ma.where(numpy.ma.getmaskarray(Ws["Data"]), ones, zeros)
     pfp_utils.CreateVariable(ds, Ws)
     return 1
 def Wd_from_Ux_Uy(ds, Wd_out, Ux_in, Uy_in):
@@ -31,9 +37,28 @@ def Wd_from_Ux_Uy(ds, Wd_out, Ux_in, Uy_in):
     Author: PRI
     Date: August 2023
     """
+    nrecs = int(ds.root["Attributes"]["nc_nrecs"])
+    zeros = numpy.zeros(nrecs, dtype=numpy.int32)
+    ones = numpy.ones(nrecs, dtype=numpy.int32)
     Ux = pfp_utils.GetVariable(ds, Ux_in)
     Uy = pfp_utils.GetVariable(ds, Uy_in)
-    _, Wd = pfp_utils.convert_UVtoWSWD(Ux, Uy)
-    Wd["Label"] = Wd_out
+    attr = {"long_name": "Wind direction", "units": "degrees",
+            "statistic_type": "average"}
+    Wd = pfp_utils.CreateEmptyVariable(Wd_out, nrecs, attr=attr)
+    if ((Ux["Attr"]["instrument"] in ["WindMaster-Pro"]) and
+        (Uy["Attr"]["instrument"] == Ux["Attr"]["instrument"])):
+        Wd_sonic = numpy.ma.mod(360 - numpy.degrees(numpy.ma.arctan2(Uy, Ux)), 360)
+        Wd["Attr"]["instrument"] = Ux["Attr"]["instrument"]
+    elif ((Ux["Attr"]["instrument"] in ["CSAT", "CSAT3A", "CSAT3B"]) and
+          (Uy["Attr"]["instrument"] == Ux["Attr"]["instrument"])):
+        Wd_sonic = numpy.ma.mod(180 - numpy.degrees(numpy.ma.arctan2(Uy, Ux)), 360)
+        Wd["Attr"]["instrument"] = Ux["Attr"]["instrument"]
+    else:
+        msg = " Unrecognised sonic anemometer type (" + Ux["instrument"]
+        msg += Uy["instrument"] + ")"
+        logger.warning(msg)
+    # we want the direction FROM which the wind blows
+    Wd["Data"] = numpy.ma.mod(Wd_sonic + 180, 360)
+    Wd["Flag"] = numpy.ma.where(numpy.ma.getmaskarray(Wd["Data"]), ones, zeros)
     pfp_utils.CreateVariable(ds, Wd)
     return 1
